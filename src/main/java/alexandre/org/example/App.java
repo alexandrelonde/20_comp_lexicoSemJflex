a08e663f46fe9a38f444e5950059afd332e6b313
Alexandre Londe <alexandrelonde.x@gmail.com>
2023-04-23 19:44:01 -0300

    feat: incrementando funções do lexico

--- src/main/java/alexandre/org/example/App.java
+++ src/main/java/alexandre/org/example/App.java
@@ -1,5 +1,11 @@
 package alexandre.org.example;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,43 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String nomeArquivo = "teste.pas";
+
+        substituirTabulacao(nomeArquivo);
+
+        Lexico lexico = new Lexico(nomeArquivo);
+
+        Token token;
+        int linha = 1;
+        int coluna = 1;
+
+        do {
+            token = lexico.getToken(linha, coluna);
+            System.out.println(token);
+            coluna = token.getColuna()+token.getTamanhoToken();
+            linha = token.getLinha();
+        } while (token.getClasse() != Classe.cEOF);
+
+    }
+
+    public static void substituirTabulacao(String nomeArquivo) {
+        Path caminhoArquivo = Paths.get(nomeArquivo);
+        int numeroEspacosPorTab = 4;
+        StringBuilder juntando = new StringBuilder();
+        String espacos;
+
+        for (int cont = 0; cont < numeroEspacosPorTab; cont++) {
+            juntando.append(" ");
+        }
+        espacos = juntando.toString();
+
+        String conteudo;
+        try {
+            conteudo = new String(Files.readAllBytes(caminhoArquivo), StandardCharsets.UTF_8);
+            conteudo = conteudo.replace("\t", espacos);
+            Files.write(caminhoArquivo, conteudo.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
